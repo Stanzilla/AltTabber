7fe1c206e761ac1c3e84d6713f496983703c74aa
Benjamin Staneck <staneck@gmail.com>
2017-10-21 10:42:39 +0200

    whatever bullshit I had local

--- AltTabber.lua
+++ AltTabber.lua
@@ -15,62 +15,32 @@ This source code is released under All Rights Reserved.
 ]]
 
 local LibStub = _G.LibStub
-
 local MODNAME	= "AltTabber"
-
-AltTabber		= LibStub("AceAddon-3.0"):NewAddon(MODNAME, "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0")
-
+AltTabber		= LibStub("AceAddon-3.0"):NewAddon(MODNAME, "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0", "AceTimer-3.0")
 local addon		= LibStub("AceAddon-3.0"):GetAddon(MODNAME)
-local AL3		= LibStub("AceLocale-3.0")
-
-local L = AL3:GetLocale(MODNAME, false)
-
---@alpha@
-_G.AT = addon
---@end-alpha@
-
 local GetCVar = GetCVar
 local SetCVar = SetCVar
-local PlaySoundFile = PlaySoundFile
+local PlaySound = PlaySound
 local GetCurrentMapAreaID = GetCurrentMapAreaID
 
 local BrawlerLocationEnabled = false
 
--------------------------------------------------------------------------------
--- Check to see if we have mandatory libraries loaded. If not, notify the user
--- which are missing and return.
--------------------------------------------------------------------------------
-local MissingLibraries
-do
-	local REQUIRED_LIBS = {
-		"AceLocale-3.0",
-		"AceEvent-3.0",
-		"AceConsole-3.0",
-		"AceHook-3.0",
-	}
-	function MissingLibraries()
-		local missing = false
-
-		for idx, lib in ipairs(REQUIRED_LIBS) do
-			if not LibStub:GetLibrary(lib, true) then
-				missing = true
-				addon:Print(strformat(L["MISSING_LIBRARY"], lib))
-			end
-		end
-		return missing
-	end
-end -- do
-
-if MissingLibraries() then
-	--@debug@
-	addon:Print("You are using a development version of Alt-Tabber.  As per WowAce standards, externals are not set up.  You will have to install all necessary libraries in order for the addon to function correctly.")
-	--@end-debug@
-	_G.AltTabber = nil
-	return
+function addon:OnInitialize()
 end
 
-function addon:OnInitialize()
-
+function addon:Scream()
+	local oldVolume = GetCVar("Sound_MasterVolume") or 0.02
+	if IsInInstance() ~= nil then
+		SetCVar("Sound_MasterVolume", 0.03)
+	else
+		SetCVar("Sound_MasterVolume", 0.04)
+	end
+	self:ScheduleTimer("Whisper", 4)
+	FlashClientIcon()
+end
+function addon:Whisper()
+	SetCVar("Sound_MasterVolume", 0.02)
+	addon:Print(GetCVar("Sound_MasterVolume"))
 end
 
 -- Checks the CVars to determine if we can actually play sound.
@@ -82,31 +52,31 @@ local function CheckCVars(sound_on)
 	local Sound_EnableSoundWhenGameIsInBG = GetCVar("Sound_EnableSoundWhenGameIsInBG")
 
 	-- If our master volume is set to 0.0 then we won't hear a damn thing.
-	if (Sound_MasterVolume == 0.0) then
-		addon:Print(L["MASTERSOUNDOFF"])
+	if (Sound_MasterVolume == 0.0) or (Sound_EnableAllSound == "0") then
+		addon:Print("Master Sound is off.")
 	end
 
 	-- If sound is off, we want to play the readycheck
-	if ((Sound_EnableSFX == "0") and (not sound_on)) then
+	if (Sound_EnableSFX == "0") and not sound_on then
 		-- If background sound is off, we can't do anything
 		-- Set the background sound to on and inform the user
 		if (Sound_EnableSoundWhenGameIsInBG == "0") then
-			addon:Print(L["BGSNDON"])
+			addon:Print("Backgrounds Sound is off and will be enabled now")
 			SetCVar("Sound_EnableSoundWhenGameIsInBG", "1")
 			return false
 
 		-- If the entire sound processing is off, we can't play sound.
 		-- Set the variables and inform the user
 		elseif (Sound_EnableAllSound == "0") then
-			addon:Print(L["ENABLESOUNDSYSTEM"])
+			addon:Print("Sound was off, will be enabled now.")
 			SetCVar("Sound_EnableAllSound","1")
 			-- Disable all the other types of sounds
 			SetCVar("Sound_EnableSFX","0")
 			SetCVar("Sound_EnableAmbience","0")
 			SetCVar("Sound_EnableMusic","0")
 			return false
-		-- We just have sound off, but conditions are correct so we will
-		-- hear the sounds.
+			-- We just have sound off, but conditions are correct so we will
+			-- hear the sounds.
 		else
 			return true
 		end
@@ -124,6 +94,7 @@ end
 -- Plays the PVP queue popping sound.
 local function PlayPVPSound(sound_on)
 	if CheckCVars(sound_on) then
+		addon:Scream()
 		PlaySound(PlaySoundKitID and "PVPEnterQueue" or 8458, "Master")
 	end
 end
@@ -131,6 +102,7 @@ end
 -- Plays the ready check sound
 local function PlayReadyCheck(sound_on)
 	if CheckCVars(sound_on) then
+		addon:Scream()
 		PlaySound(PlaySoundKitID and "ReadyCheck" or 8960, "Master")
 	end
 end
